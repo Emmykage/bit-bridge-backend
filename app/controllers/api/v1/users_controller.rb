dd5325c7b29754a04a67932614c6d73375fa7e42
morris menanya <emmiemenz@gmail.com>
2024-12-14 21:04:37 +0100

    add user controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,11 @@
+class Api:V1::UsersController < ApplicationController
+  def index
+
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/update'
+  get 'users/delete'
   # devise_for :users
   devise_for :users, path: "", path_names: {
     sign_in: "login",
