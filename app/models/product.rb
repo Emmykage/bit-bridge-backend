c6cd74e9f674a9d17c962fedcb2a39a688a05033
morris menanya <emmiemenz@gmail.com>
2024-12-24 04:29:29 +0100

    add credential

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,3 +10,5 @@ class Product < ApplicationRecord
       end
 
 end
+
+# bbc9f42b3aae0f9c00e9b53ec48588aa3d32db131e118dd8955b3394b547e9a13572f1b499caf7cccb0bc4e61b080d82fc056321b56eb01bcac62fb6bcd0191c
