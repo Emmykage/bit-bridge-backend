7b3ec18db8928b920fd88a7d8eb3f37186903998
morris menanya <emmiemenz@gmail.com>
2025-01-15 16:56:11 +0100

    ad delete

--- app/models/provision.rb
+++ app/models/provision.rb
@@ -1,5 +1,5 @@
 class Provision < ApplicationRecord
-  belongs_to :product
+  belongs_to :product, dependent: :destroy
   has_many :order_items
   enum :currency,  {ngn: 0, usd: 1, gbp: 2, eur: 3}
 
