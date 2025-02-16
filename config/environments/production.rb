5a69b317f307c6137b7fb39dcd5b70001197e1e1
morris menanya <emmiemenz@gmail.com>
2025-02-16 07:59:39 +0100

    replace username and password with env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,8 +17,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.hostinger.com",
     :port                 => 587,
-    :user_name            => "support@bitbridgeglobal.com",
-    :password             => "@SupportBitbridgeglobal123",
+    :user_name            =>  ENV["SMTP_USERNAME"],
+    :password             => ENV["SMTP_PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
