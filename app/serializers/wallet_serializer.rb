c98266b7fa561050195ac821bc4eb92e4f942d1e
morris menanya <emmiemenz@gmail.com>
2025-05-06 02:49:21 +0100

    add total deposit

--- app/serializers/wallet_serializer.rb
+++ app/serializers/wallet_serializer.rb
@@ -1,5 +1,5 @@
 class WalletSerializer < ActiveModel::Serializer
-  attributes :id, :balance, :wallet_type, :total_bills, :withdrawn
+  attributes :id, :balance, :wallet_type, :total_bills, :withdrawn, :total_deposit
   has_one :user
   has_many :transactions
 end
