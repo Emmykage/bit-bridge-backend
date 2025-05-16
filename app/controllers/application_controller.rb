b3cfce5861bdaca54a847826b4a68e6e23571761
morris menanya <emmiemenz@gmail.com>
2025-05-16 17:43:39 +0100

    ADD PHONE NUMBER TO TV

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::API
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: %i[role])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:role,
+        user_profile_attributes: %i[last_name first_name phone_number]])
         devise_parameter_sanitizer.permit(:account_update, keys: %i[screen_name])
     end
 
