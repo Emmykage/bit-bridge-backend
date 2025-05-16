00e1b8ed032f7defb408c26acb73a984c40c2762
morris menanya <emmiemenz@gmail.com>
2025-05-16 17:54:02 +0100

    fix error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
   include RackSessionFix
 
-  priv  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -64,7 +64,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
 
-ate
+
 
   def respond_with(resource, _opts = {})
 
