a39bd7b993f1ed5ab2aafee2b58d91ef73ccd1c5
morris menanya <emmiemenz@gmail.com>
2025-07-14 19:29:16 +0100

    update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -125,7 +125,7 @@ end
 
 
   def user_params
-    params.require(:user).permit(:email, :password, :old_password, :confirm_password, :password_token, user_profile_attributes: [:id, :first_name, :last_name, :phone_number])
+    params.require(:user).permit(:email, :acive, :password, :old_password, :confirm_password, :password_token, user_profile_attributes: [:id, :first_name, :last_name, :phone_number])
   end
 
   def generate_reset_token(user)
