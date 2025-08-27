3d51465789b5d3dc1b3eba74ec99de99f488adad
morris menanya <emmiemenz@gmail.com>
2025-08-27 18:59:02 +0100

    addd controller

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -7,8 +7,12 @@ module Users
     def show
       self.resource = resource_class.confirm_by_token(params[:confirmation_token])
       if resource.errors.empty?
+        user = resource
+
+        new_refresh_token = user.generate_refresh_token
+        access_token, _payload = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil)
         puts '✅ successful confirmation — redirect to your React app'
-        render json: { message: 'User confirmed' }, status: :ok
+        render json: { message: 'User confirmed', refresh_token: new_refresh_token, access_token: access_token  }, status: :ok
       else
         # ❌ failed confirmation — redirect with error message
         render json: { message: 'Failed to Confirm' }, status: :unprocessable_entity
