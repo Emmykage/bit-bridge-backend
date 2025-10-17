6b0c9a559f0843ec850c7a711440142c636da6cd
morris menanya <emmiemenz@gmail.com>
2025-10-17 22:08:14 +0100

    dev: fix environment front end url

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -5,7 +5,8 @@ class CustomDeviseMailer < Devise::Mailer
   default from: 'support@bitbridgeglobal.com'
   def confirmation_instructions(record, token, opts = {})
     # Customize the confirmation email
-    frontend_url = "https://bitbridgeglobal.com/confirmation?confirmation_token=#{token}"
+    base_url = Rails.application.credentials.dig(:frontend_url) || ENV['FRONTEND_URL'] || 'https://bitbridgeglobal.com/'
+    frontend_url = "#{base_url}confirmation?confirmation_token=#{token}"
     attachments.inline['logo'] = File.read(Rails.root.join('app/assets/images/logo1.png'))
 
     opts[:subject] = 'Confirm your account'
@@ -13,15 +14,7 @@ class CustomDeviseMailer < Devise::Mailer
     @token = token
     @user = record
     mail(to: record.email, subject: opts[:subject])
-    #   do |format|
-    #     format.html do
-    #       render html: <<~HTML.html_safe
-    #         <h1>Welcome to BitBridge Global!</h1>
-    #         <p>Please confirm your account by clicking the link below:</p>
-    #         <p><a href='#{@confirmation_link}'>Confirm My Account</a></p>
-    #       HTML
-    #     end
-    #   end
+
   end
 
   def reset_password_instructions(record, token, opts = {})
