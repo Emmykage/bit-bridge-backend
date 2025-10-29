cbe0728c1b2a2e013e554e6906748b0acd4d8bf9
morris menanya <emmiemenz@gmail.com>
2025-10-29 13:25:25 +0100

    update

--- db/migrate/20251029070806_rename_posta_amount_to_postal_code.rb
+++ db/migrate/20251029070806_rename_posta_amount_to_postal_code.rb
@@ -2,6 +2,6 @@
 
 class RenamePostaAmountToPostalCode < ActiveRecord::Migration[7.1]
   def change
-    rename_column :cards, :posta, :postal_code
+    # rename_column :cards, :posta, :postal_code
   end
 end
